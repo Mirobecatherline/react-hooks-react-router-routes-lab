82d95efebd2f9a174b3d97f87fd87d17815768db
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-07-23 23:01:21 +0300

    commit tree 115af0c9de514fa911f63f5dcd95655a649616f3

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { actors } from "../data";
+import { actors } from "../data"
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
+function Actors(){
+  const actorlist = actors.map((list) => (
+    <div key={list.name}>
+      <h1>{list.name}</h1>
+      <ul>
+        {list.movies.map((list2) => (
+          <li key={list2}>{list2}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorlist}
+    </div>
+  );
+};
 
 export default Actors;
