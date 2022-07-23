7a1db9521bc6489d4408c4030d01bebcd2fcb8d1
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-07-23 23:01:30 +0300

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { directors } from "../data";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+function Directors(){
+  const directorlist = directors.map((list4) => (
+    <div key={list4.name}>
+      <h2>{list4.name}</h2>
+      <ul>
+        {list4.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorlist}
+    </div>
+  );
+};
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+function Movies(){
+  const movieItems = movies.map((list) => (
+    <div key={list.title}>
+      <h2>{list.title}</h2>
+      <p>{list.time}</p>
+      <ul>
+        {list.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieItems}
+    </div>
+  );
+};
 
 export default Movies;
